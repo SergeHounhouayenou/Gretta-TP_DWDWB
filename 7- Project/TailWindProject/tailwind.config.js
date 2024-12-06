f14929865c351b8fac465eca7a27116e75709935
SergeHounhouayenou <120483968+SergeHounhouayenou@users.noreply.github.com>
2024-12-06 16:59:12 +0100

    Taillwindcss start

--- 7- Project/TailWindProject/tailwind.config.js
+++ 7- Project/TailWindProject/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+          colors:{
+              primary: '#1D4ED8', //bleu principal
+              secondary: "#9333EA", //Violet secondaire
+              accent: "#F59E0B",
+          },
+          fontFamily:{
+            heading: ['Poppins', 'sans-serif'],
+          },
+          animation:{
+            fadeIn: 'fadeIn 1s ease-in-out',
+          },
+          keyframes:{
+            fadeIn:{ 
+              '0%': {opacity:0, transform:'translateY(20px)'},
+            '100%': { opacity:1, transform: 'translateY(0)'},
+                   },
+        },
+          
+        
+      },
   },
   plugins: [],
 }
